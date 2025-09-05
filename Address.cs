f7c8d5172ba035301a4326a98abe40c9badc15fe
Clemens Österle <clemens.Oesterle@omicronenergy.com>
2025-09-05 09:39:52 +0200

    Add project files.

---
+++ Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonData.MinVer.Spike
+{
+    /// <inheritdoc/>
+    public class Address : IAddress
+    {
+        /// <inheritdoc/>
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ IAddress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonData.MinVer.Spike
+{
+    /// <summary>
+    /// Represents an address with a name associated with it.
+    /// </summary>
+    /// <remarks>This interface defines a contract for objects that have a name property,  typically used to
+    /// represent an address or related entity.</remarks>
+    public interface IAddress
+    {
+        /// <summary>
+        /// Gets or sets the name associated with the object.
+        /// </summary>
+        string Name { get; set; }
+    }
+}
